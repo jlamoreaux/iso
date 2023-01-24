e3c7006a85c17d01a2403393aee0145ae4dfc4c1
Jordan Lamoreaux <jlamoreaux@atlassian.com>
2023-01-24 12:57:21 -0600

    fix inbox bug

--- packages/frontend/src/pages/messages/Inbox.tsx
+++ packages/frontend/src/pages/messages/Inbox.tsx
@@ -1,26 +1,36 @@
 import { Container, Loader, Stack, Text, Title } from "@mantine/core";
-import React from "react";
+import React, { useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthWrapper } from "../../context/AuthProvider";
 import MessageTile from "../../components/cards/MessageTile";
 import { IncomingMessage } from "../../services/api";
 
+const sortMessages = (messages: IncomingMessage[]) => {
+  if (messages.length > 0) {
+    return messages.sort((a: any, b: any) => {
+      return new Date(b.lastReply).valueOf() - new Date(a.lastReply).valueOf();
+    });
+  }
+};
+
 // Message inbox page
 const Inbox: React.FC = () => {
   const unsortedMessages = useLoaderData() as IncomingMessage[];
-
-  const messages = unsortedMessages.sort((a: any, b: any) => {
-    return new Date(b.lastReply).valueOf() - new Date(a.lastReply).valueOf();
-  });
+  let sortedMessages: IncomingMessage[] | undefined;
+  if (unsortedMessages) {
+    sortedMessages = sortMessages(unsortedMessages);
+  }
 
   return (
     <AuthWrapper>
       <Container>
         <Title>INBOX</Title>
         <Stack spacing="md">
-          {messages &&
-            messages.length > 0 &&
-            messages.map((message: any, i: number) => <MessageTile message={message} key={i} />)}
+          {sortedMessages &&
+            sortedMessages.length > 0 &&
+            sortedMessages.map((message: any, i: number) => (
+              <MessageTile message={message} key={i} />
+            ))}
         </Stack>
       </Container>
     </AuthWrapper>
